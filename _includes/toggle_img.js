75eba08d3dfff625cd93ab78056fac7f2710e750
Adam Cordingley <AdamC19@users.noreply.github.com>
2018-02-28 15:48:05 -0500

    Add working code for the image-toggle function

--- _includes/toggle_img.js
+++ _includes/toggle_img.js
@@ -1,13 +1,27 @@
-//<button onclick="toggleContent('What time is it?','time')">The time is?</button>
 
-function toggleContent(text1, text2) {
-    var text = document.getElementById("demo").innerHTML;
-    if(text == text1){
-    	text = text2;
-    }else if(text == text2){
-    	text = text1;
-    }else{
-    	text = text1;
-    }
-    document.getElementById("demo").innerHTML = text;
+/*
+ *  Intended for use with a button object, called by the onclick event.
+ *  EX:
+ *      <img id="toggle">
+ *      <button onclick="toggle('toggle', 'small.png', 'big.png')">Click to view other image</button>
+ *
+ *  @param id : the ID of the img tag, the contents of which we want to toggle
+ *  @param small : the url of the small image (the image preview)
+ *  @param big : the url of the large image (full size image)
+ */
+function toggleImg(id, small, big){
+	var targetNode 	= document.getElementById(id); 	        // get this HTML Object
+	var attribs 	= targetNode.attributes; 				// get the NamedNodeMap
+	var imgName 	= attribs.getNamedItem("src").value; 	// gives us the img src (i.e. "img_name.png" )
+
+	if(imgName == small){
+		imgName = big;      // give the target the big img 
+	}else if(imgName == big){
+		imgName = small;    // give the target the small img
+	}else{
+		imgName = small;    // give the target the small img as a default
+	}
+	
+    // finally, set the src attribute
+	attribs.getNamedItem("src").value = imgName;
 }
